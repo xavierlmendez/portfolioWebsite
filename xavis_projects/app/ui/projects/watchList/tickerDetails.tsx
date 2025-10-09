0b6481b09535c1d030684d97a527982ffacf8974
xaviermendez <xluis10.2@gmail.com>
2025-10-09 06:40:27 -0500

    accounting for mobile view with ticker details display

--- xavis_projects/app/ui/projects/watchList/tickerDetails.tsx
+++ xavis_projects/app/ui/projects/watchList/tickerDetails.tsx
@@ -20,37 +20,37 @@ type TickerProps = {
     return (
     <li className='mb-4 w-full overflow-hidden rounded-xl shadow-lg transition hover:shadow-xl'>
       <Link href={`watchList/details/${update.id}`} className='block bg-white p-6 rounded-xl hover:bg-gray-50'>
-        <div className='grid grid-cols-7 items-center gap-6 text-gray-800'>
-          <h2 className={`text-xl font-bold ${titleColor}`}>
+        <div className="grid grid-cols-1 md:grid-cols-7 items-center gap-6 text-gray-800">
+          <h2 className={`text-xl font-bold ${titleColor} col-span-1 md:col-span-1 text-left md:text-left`}>
             {update.title}
           </h2>
 
-          <div className='text-center'>
+          <div className='text-left md:text-center'>
             <span className='block font-semibold'>Price</span>
             <span>{update.price.replace('Price: ', '')}</span>
           </div>
 
-          <div className='text-center'>
+          <div className="text-left md:text-center truncate">
             <span className='block font-semibold'>Change</span>
-            <span>{update.change.replace('Change: ', '')}</span>
+            <span >{update.change.replace('Change: ', '')}</span>
           </div>
 
-          <div className='text-center'>
+          <div className="text-left md:text-center truncate">
             <span className='block font-semibold'>Volume</span>
             <span>{update.volume.replace('Volume: ', '')}</span>
           </div>
 
-          <div className='text-center'>
+          <div className="text-left md:text-center truncate">
             <span className='block font-semibold'>Bid</span>
             <span>{update.bid.replace('Bid: ', '')}</span>
           </div>
 
-          <div className='text-center'>
+          <div className="text-left md:text-center truncate">
             <span className='block font-semibold'>Ask</span>
             <span>{update.ask.replace('Ask: ', '')}</span>
           </div>
 
-          <div className='text-center'>
+          <div className="text-left md:text-center truncate">
             <span className='block font-semibold'>Day Range</span>
             <span>{update.dayRange.replace('Day Range: ', '')}</span>
           </div>
