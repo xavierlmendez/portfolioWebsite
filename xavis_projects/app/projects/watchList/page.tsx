348fd0f76878cc24f1c0959d01b26afc8e5043e7
Xavier Mendez <44298432+xavierlmendez@users.noreply.github.com>
2025-10-20 16:27:36 -0500

    mobile changes

--- xavis_projects/app/projects/watchList/page.tsx
+++ xavis_projects/app/projects/watchList/page.tsx
@@ -9,7 +9,7 @@ export default async function WatchList() {
   const initTickerData = await getWatchListUpdates(initTickerSymbols)
 
   return (
-    <main className='w-full min-h-screen flex flex-col items-center md:px-4 py-8'>
+    <main className='w-full min-h-screen flex flex-col items-center sm:px-4 py-8'>
       <section id='header' className='w-full max-w-3xl mb-8 text-center'>
         <h1 className='text-4xl font-bold text-white mb-2'>Watch List Tracker</h1>
         <p className='text-lg text-gray-300'>{summary}</p>
